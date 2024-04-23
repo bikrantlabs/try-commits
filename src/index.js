cf708939cbfc70235ae80b40b24f3c2063a17e96
Bikrant Jung <bikrantjung1214@gmail.com>
2024-04-23 16:14:15 +0545

    fix: bug fix

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 console.log("This is another feature yet");
+console.log("Bug has now been fixed!!");
--- src/new-feature.js
+++ src/new-feature.js
@@ -1,2 +1,4 @@
 console.log("This is a new feature");
 console.log("This is a second feature");
+
+console.log("Bug is fixed here too...");
